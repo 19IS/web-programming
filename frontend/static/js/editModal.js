0760cea09363c0de422171aa74296254128ef73e
Hunter-99 <andreytitan2001@gmail.com>
2023-04-04 16:26:41 +0300

    Updated `editModal.js`

--- frontend/static/js/editModal.js
+++ frontend/static/js/editModal.js
@@ -1,5 +1,6 @@
 function getPostInfo(postId) {
-    fetch('http://127.0.0.1:3000/blog/posts/' + postId, {
+    return new Promise((resolve, reject) => {
+        fetch('http://127.0.0.1:3000/blog/posts/' + postId, {
         method: 'GET',
         headers: { 
             'Content-Type': 'application/json' 
@@ -9,9 +10,10 @@ function getPostInfo(postId) {
     ).then(
         data => {
             console.log(data)
-            return data
+            resolve(data);
         }
     )
+    });
 };
 
 
@@ -22,31 +24,32 @@ document.addEventListener('DOMContentLoaded', () => {
     editModal.addEventListener('show.bs.modal', event => {
         const button = event.relatedTarget;
         const postId = button.getAttribute('data-bs-id');
-        const modalTitle = editModal.querySelector('input#title-name');
-        const modalAuthor = editModal.querySelector('input#author-name');
-        const modalBlogText = editModal.querySelector('textarea#blog-text');
-        const postInfo = getPostInfo(postId);
-
+        
+        getPostInfo(postId).then((postInfo) => {
+            editModal.querySelector('input#title-name').value = postInfo['title']
+            editModal.querySelector('input#author-name').value = postInfo['author']
+            editModal.querySelector('textarea#blog-text').value = postInfo['content']
+            
         editModal.querySelector('h1#editModalLabel').textContent = `Редактивировать пост с id = ${postId}`;
 
         editModal.addEventListener('hidden.bs.modal', () => {
             console.log('modal element completely hidden!');
         });
-
+        
         editModal.querySelector('.btn-primary').addEventListener('click', (event) => {
             res = fetch('http://127.0.0.1:3000/blog/posts', {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    author: modalAuthor.value,
-                    title: modalTitle.value,
-                    content: modalBlogText.value,
+                    author: editModal.querySelector('input#title-name').value,
+                    title: editModal.querySelector('input#author-name').value,
+                    content: editModal.querySelector('textarea#blog-text').value,
                     _id: postId
                 })
             }).then(() => {
                 window.location.href = "http://127.0.0.1:3000/blog";
             }).catch(error => console.log(error));
         });
-
+    });
     });
 });
